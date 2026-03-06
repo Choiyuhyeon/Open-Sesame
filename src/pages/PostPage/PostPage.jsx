0fb523a41043294a1acf2511777db87e077ae27d
imyoonsoo <bliss38soo@gmail.com>
2026-03-06 14:33:58 +0900

    refactor: hook으로 공유 기능 분리

--- src/pages/PostPage/PostPage.jsx
+++ src/pages/PostPage/PostPage.jsx
@@ -2,6 +2,7 @@ import '../PostPage/PostPage.css';
 import PostHeader from './components/PostHeader';
 import Profile from '../../assets/images/Profile.png';
 import { useState, useEffect } from 'react';
+import { useShare } from '../../hooks/useShare';
 
 function PostPage() {
   const [data, setData] = useState({
@@ -9,56 +10,16 @@ function PostPage() {
     profile: Profile,
   });
 
-  const [isToast, setIsToast] = useState(false); // 토스트 기본 설정 -> false
-
-  const CopyLink = () => {
-    const currentUrl = window.location.href;
-    navigator.clipboard.writeText(currentUrl);
-    setIsToast(true);
-    setTimeout(() => setIsToast(false), 5000);
-  };
-
-  const ShareKakao = () => {
-    if (window.Kakao) {
-      window.Kakao.Share.sendDefault({
-        objectType: 'feed',
-        content: {
-          title: '야옹이',
-          description: '신나는 노래 추천 받습니다!!!!!',
-          imageUrl:
-            'https://images.pet-friends.co.kr/v2/community/2025/11/16/f80f0c50-abed-431b-ad7c-63b1007b7eab.jpeg?f=webp',
-          link: {
-            mobileWebUrl: window.location.href,
-            webUrl: window.location.href,
-          },
-        },
-        buttons: [
-          {
-            title: '웹으로 보기',
-            link: {
-              mobileWebUrl: window.location.href,
-              webUrl: window.location.href,
-            },
-          },
-        ],
-      });
-    }
-  };
-
-  const ShareFacebook = () => {
-    const currentUrl = window.location.href;
-    window.open(
-      `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(currentUrl)}`
-    );
-  };
+  const [isToast, setIsToast] = useState(false); // 토스트 기본 설정 false
+  const { copyLink, shareKakao, shareFacebook } = useShare(setIsToast);
 
   return (
     <>
       <PostHeader
         {...data}
-        onClick_Link={CopyLink}
-        onClick_Kakao={ShareKakao}
-        onClick_Facebook={ShareFacebook}
+        onClick_Link={copyLink}
+        onClick_Kakao={shareKakao}
+        onClick_Facebook={shareFacebook}
       />
       {isToast && <div className="toast-msg">URL이 복사되었습니다!</div>}
     </>

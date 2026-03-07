0b3d42564e6a9c0c01c790b18c7da4076e092c88
최문경 <mkv9312101777@gmail.com>
2026-03-07 10:17:16 +0900

    feat: homepage 구현

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,5 +1,24 @@
+import './HomePage.css';
+import GoQuestionButton from './components/GoQuestionButton/GoQuestionButton';
+import LogoBotton from './components/LogoBotton/LogoBotton';
+import QuestionForm from './components/QuestionForm/QuestionForm';
+import papercupimg from '../../assets/images/papercupimg.png';
+
 function HomePage() {
-  return <div>Home Page</div>;
+  return (
+    <div className="home-page">
+      <div className="top-area">
+        <GoQuestionButton />
+      </div>
+
+      <div className="main-area">
+        <LogoBotton />
+        <QuestionForm />
+      </div>
+
+      <img src={papercupimg} alt="" className="papercup-image" />
+    </div>
+  );
 }
 
 export default HomePage;
